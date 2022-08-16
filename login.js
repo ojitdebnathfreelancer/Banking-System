aadb649875a9e50a74a1eda04dabf13b58fa1016
unknown <ojitdebnathfreelancer.com>
2022-08-16 21:44:47 +0600

    url change

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ document.getElementById('submit-btn').addEventListener('click', function(){
     const userMail = document.getElementById('user-mail');
     const userPassword = document.getElementById('user-password');
     if( userMail.value === 'user@gmail.com' && userPassword.value === 'userlogin'){
-        window.location.href = 'http://127.0.0.1:5500/deposit.html';
+        window.location.href = '/deposit.html';
     } 
     else{
         alert('Please type valid mail and password');
